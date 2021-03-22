38daec50bff48d86c814fef501a4caba0cf99fbe
Stephanie Murphy <smurphy@mas.local>
2021-03-22 02:07:23 -0400

    changes

--- schema.sql
+++ schema.sql
@@ -12,17 +12,15 @@ CREATE TABLE department(
 
 CREATE TABLE role(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
+    title VARCHAR(30),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department (id) 
 );
 
 CREATE TABLE employee(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT NOT NULL,
     manager_id INT,
-     FOREIGN KEY (id) REFERENCES role (id)
 );
